bfeedee90e63f0fe31291d6418330a37fd011ecf
kristobalus <ildar.tamendarov@gmail.com>
2024-08-30 13:46:29 +0500

    update

---
+++ bash.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+docker container run -it --rm \
+--runtime=nvidia --gpus all \
+--privileged \
+--device /dev/ttyTHS0 \
+--device /dev/ttyTHS1 \
+--device /dev/ttyTHS4 \
+-v ./:/opt \
+testimg /bin/bash
