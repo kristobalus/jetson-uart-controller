b0e38d75aabeedaffa84753a6becf11b0db71cdb
kristobalus <ildar.tamendarov@gmail.com>
2024-09-02 18:37:39 +0500

    feat: i2c support

--- test-i2c.py
+++ test-i2c.py
@@ -1,17 +1,25 @@
 import time
-from tfmini_i2c import TFminiI2C
-
+from smbus2 import SMBus, i2c_msg
+import time
 
 def main():
-    sensor1 = TFminiI2C(0, 0x50)
-    sensor2 = TFminiI2C(0, 0x57)
-    while True:
-        data1 = sensor1.readAll()
-        data2 = sensor2.readAll()
-        print(data1)
-        print(data2)
-        print("--------")
-        time.sleep(1)
+    address = 0x50
+    # write Reg_H, Reg_L and Data Length to the sensor, without (!) a STOP
+    # request read, specifying the sensor and 7 bytes
+    write = i2c_msg.write(address, [1, 2, 7])
+    read = i2c_msg.read(address, 7)
+
+    with SMBus(1) as bus:
+        bus.i2c_rdwr(write, read)
+        data = list(read)
+        TrigFlag = data[0]
+        Dist = (data[3] << 8 | data[2])
+        Strength = (data[5] << 8 | data[4])
+        Mode = (data[6])
+
+        print(Dist)
+        print(Strength)
+        print(Mode)
 
 try:
     main()
