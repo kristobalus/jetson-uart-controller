58f3f121f7289eb687a1579bc8248ab4eace3933
kristobalus <ildar.tamendarov@gmail.com>
2024-09-02 18:48:37 +0500

    update

--- test-i2c.py
+++ test-i2c.py
@@ -4,8 +4,8 @@ from TFminiI2C import TFminiI2C
 
 try:
     while True:
-        sensor0 = TFminiI2C(1, 0x50)
-        sensor1 = TFminiI2C(1, 0x57)
+        sensor0 = TFminiI2C(0, 0x50)
+        sensor1 = TFminiI2C(0, 0x57)
         print(sensor0.readDistance())
         print(sensor1.readDistance())
         time.sleep(0.1)
