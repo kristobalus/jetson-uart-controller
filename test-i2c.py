9538cec1ff95b4be68088dca25b6d226d992effa
kristobalus <ildar.tamendarov@gmail.com>
2024-09-02 19:51:06 +0500

    update

--- test-i2c.py
+++ test-i2c.py
@@ -2,10 +2,10 @@ from smbus2 import SMBus, i2c_msg
 import time
 
 # I2C address of the TFmini Plus (default is 0x10)
-I2C_ADDRESS = 0x10
+I2C_ADDRESS = 0x50
 
 # Initialize the I2C bus (use the correct bus number, e.g., 1 for Raspberry Pi)
-bus = SMBus(1)
+bus = SMBus(0)
 
 # Command to obtain the firmware version
 command = [0x5A, 0x04, 0x01, 0x5F]
