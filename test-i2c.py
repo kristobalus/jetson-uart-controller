67be3093631d326267b902e30871007c160a4c1e
kristobalus <ildar.tamendarov@gmail.com>
2024-09-02 16:48:02 +0500

    feat: i2c support

--- test-i2c.py
+++ test-i2c.py
@@ -5,7 +5,7 @@ bus = smbus2.SMBus()  # or bus = smbus2.SMBus(1) e.g 1 for Raspberry Pie
 bus.open(0)
 
 # I2C address of the device
-address = 0x50  # replace with your device's I2C address
+address = 0x10  # replace with your device's I2C address
 
 
 def read_data():
@@ -31,13 +31,13 @@ def main():
         write = i2c_msg.write(address, [1, 2, 7])
         read = i2c_msg.read(address, 7)
         bus.i2c_rdwr(write, read)
-        data = read_data()
-        if data:
-            if data[0] == 0x59 and data[1] == 0x59:  # Check header
-                distance = data[2] + data[3] * 256
-                strength = data[4] + data[5] * 256
-                print('(', distance, ',', strength, ')')
-        time.sleep(0.1)
+        read_data()
+        # if data:
+        #     if data[0] == 0x59 and data[1] == 0x59:  # Check header
+        #         distance = data[2] + data[3] * 256
+        #         strength = data[4] + data[5] * 256
+        #         print('(', distance, ',', strength, ')')
+        time.sleep(1)
 
 
 try:
