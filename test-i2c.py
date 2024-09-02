290e2aafddf95b5122154ea98d4e569646fabcab
kristobalus <ildar.tamendarov@gmail.com>
2024-09-02 17:11:55 +0500

    feat: i2c support

--- test-i2c.py
+++ test-i2c.py
@@ -1,5 +1,6 @@
 from smbus2 import smbus2, i2c_msg
 import time
+from tfmini_i2c import TFminiI2C
 
 bus = smbus2.SMBus()  # or bus = smbus2.SMBus(1) e.g 1 for Raspberry Pie
 bus.open(0)
@@ -8,33 +9,11 @@ bus.open(0)
 address = 0x50 # replace with your device's I2C address
 
 
-def read_data():
-    try:
-        # Read 8 bytes from the device
-        # data = bus.read_i2c_block_data(address, 0, 9)
-        write = i2c_msg.write(address, [1, 2, 7])
-        read = i2c_msg.read(address, 7)
-        bus.i2c_rdwr(write, read)
-        data = list(read)
-        trig_flag = data[0]
-        dist = (data[3] << 8 | data[2])
-        strength = (data[5] << 8 | data[4])
-        mode = (data[6])
-        print(f"trig_flag={trig_flag}, dist={dist}, strength={strength}, mode={mode}")
-        return data
-    except IOError as e:
-        print(f"Error reading from I2C device: {e}")
-        return None
-
-
 def main():
+    sensor = TFminiI2C(1, 0x10)
     while True:
-        read_data()
-        # if data:
-        #     if data[0] == 0x59 and data[1] == 0x59:  # Check header
-        #         distance = data[2] + data[3] * 256
-        #         strength = data[4] + data[5] * 256
-        #         print('(', distance, ',', strength, ')')
+        distance=sensor.read_distance()
+        print(f"distance={distance}")
         time.sleep(0.1)
 
 
