b6afeebf195a3445938ab6610578669cab7cc042
kristobalus <ildar.tamendarov@gmail.com>
2024-09-02 20:55:12 +0500

    update

--- test-i2c.py
+++ test-i2c.py
@@ -3,6 +3,8 @@
 from smbus2 import SMBus, i2c_msg
 import time
 
+
+bus = SMBus(0)
 address = 0x50
 
 # write Reg_H, Reg_L and Data Length to the sensor, without (!) a STOP
@@ -11,21 +13,12 @@ address = 0x50
 write = i2c_msg.write(address, [0x05, 0x00, 0x01, 0x60])
 read = i2c_msg.read(address, 9)
 
-with SMBus(0) as bus:
+
+while True:
     start = time.time()
     bus.i2c_rdwr(write, read)
     data = list(read)
-
     print(data)
-    print(data[0])
-    print(data[2] << 8 | data[1])
-    print(data[3])
-
-    # TrigFlag = data[0]
-    # Dist = ((data[3] << 8 | data[2]))
-    # Strength = ((data[5] << 8 | data[4]))
-    # Mode = (data[6])
-
-    # print(Dist)
-    # print(Strength)
-    # print(Mode)
+    dist = (data[3] << 8 | data[2])
+    print(dist)
+    time.sleep(1)
