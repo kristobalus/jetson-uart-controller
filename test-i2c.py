7484a43a06dc3476e530e12724a6f63c216c2d1f
kristobalus <ildar.tamendarov@gmail.com>
2024-09-02 18:48:22 +0500

    update

--- test-i2c.py
+++ test-i2c.py
@@ -1,27 +1,14 @@
 import time
-from smbus2 import SMBus, i2c_msg
-import time
 
-def main():
-    address = 0x50
-    # write Reg_H, Reg_L and Data Length to the sensor, without (!) a STOP
-    # request read, specifying the sensor and 7 bytes
-    write = i2c_msg.write(address, [1, 2, 7])
-    read = i2c_msg.read(address, 7)
-
-    with SMBus(0) as bus:
-        bus.i2c_rdwr(write, read)
-        data = list(read)
-        TrigFlag = data[0]
-        Dist = (data[3] << 8 | data[2])
-        Strength = (data[5] << 8 | data[4])
-        Mode = (data[6])
-
-        print(Dist)
-        print(Strength)
-        print(Mode)
+from TFminiI2C import TFminiI2C
 
 try:
-    main()
+    while True:
+        sensor0 = TFminiI2C(1, 0x50)
+        sensor1 = TFminiI2C(1, 0x57)
+        print(sensor0.readDistance())
+        print(sensor1.readDistance())
+        time.sleep(0.1)
+
 except KeyboardInterrupt:  # Ctrl+C
     print("Program interrupted")
