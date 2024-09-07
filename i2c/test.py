50a9c8ffe851490119bd6d9559382203ff6dfab0
kristobalus <ildar.tamendarov@gmail.com>
2024-09-07 15:28:23 +0500

    feat: commitizen

--- i2c/test.py
+++ i2c/test.py
@@ -20,3 +20,27 @@ while True:
     mode = data[6]
     print(dist, strength, mode)
     time.sleep(1)
+
+# ## proof of concept
+#
+# from smbus2 import SMBus, i2c_msg
+# import time
+#
+#
+# bus = SMBus(8)
+# address = 0x10
+#
+# write = i2c_msg.write(address, [0x5A, 0x05, 0x00, 0x01, 0x60])
+# read = i2c_msg.read(address, 9)
+#
+#
+# while True:
+#     start = time.time()
+#     bus.i2c_rdwr(write, read)
+#     data = list(read)
+#     print(data)
+#     dist = (data[3] << 8 | data[2])
+#     strength = data[5] << 8 | data[4]
+#     mode = data[6]
+#     print(dist, strength, mode)
+#     time.sleep(1)
